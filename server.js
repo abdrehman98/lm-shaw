f09e520cd8908077127c20d393eacdae2b8eccb6
Abdul Rehman <houneie@gmail.com>
2020-07-29 16:18:49 +0500

    Enabled Duplex Communication

--- server.js
+++ server.js
@@ -2,27 +2,49 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-let data ={
-    message : "Default"
+let data1 ={
+    message : "Default",
+    check : false
 }
-var newMessage = false
+
+let data2 ={
+    message : "Default",
+    check : false
+}
+
 
 //Root Page
 app.get('/', (req, res) => res.send('<h>This is a backend Server for Sending Hello Around the World</h>'))
 
-//Setter
-app.get('/set', (req, res) => {
-    data.message = req.query.message
-    newMessage = true
+//Setter for Dev 1
+app.get('/set1', (req, res) => {
+    data1.message = req.query.message
+    data1.check = true
     res.send("Sent")
 });
 
-//getter
-app.get('/get', (req, res) => {
-    res.send(data)
-    if(newMessage){
-        newMessage = false
-        data.message = "Default"
+//getter for Dev 1
+app.get('/get1', (req, res) => {
+    res.send(data1)
+    if(data1.check){
+        data1.check = false
+        data1.message = "Default"
+    }
+});
+
+//Setter for Dev 2
+app.get('/set2', (req, res) => {
+    data2.message = req.query.message
+    data2.check = true
+    res.send("Sent")
+});
+
+//getter for Dev 2
+app.get('/get2', (req, res) => {
+    res.send(data2)
+    if(data2.check){
+        data2.check = false
+        data2.message = "Default"
     }
 });
 
